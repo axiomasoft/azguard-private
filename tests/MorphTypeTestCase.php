704d16ba8f1eef0a6e2b34c4dfb629601f62b103
Dmitry Vostrikov <vostrikovdv@yandex.ru>
2026-07-22 07:43:31 +0300

    test(database): изолировали ULID refresh-state (P4.15)

--- tests/MorphTypeTestCase.php
+++ tests/MorphTypeTestCase.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace AzGuard\Tests;
 
+use Orchestra\Testbench\Attributes\ResetRefreshDatabaseState;
+
 /**
  * Boots the suite with ULID morph keys so the az_guard tables are migrated
  * with ulidMorphs (config-driven morph_type).
  */
+#[ResetRefreshDatabaseState]
 class MorphTypeTestCase extends TestCase
 {
     protected function getEnvironmentSetUp($app): void
